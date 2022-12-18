6fb1ce2be3dd4eb14ad6dfa28ebbc0011949a5d3
Kartika Diva Asmara Gita <90994290+divaasmara@users.noreply.github.com>
2022-12-18 14:31:06 +0700

    Update Pacman.java

--- FinalProjectPacman/src/application/Pacman.java
+++ FinalProjectPacman/src/application/Pacman.java
@@ -4,7 +4,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Pacman extends Circle {
-	
-  
+    public Pacman(double x, double y) {
+        this.setCenterX(x);
+        this.setCenterY(y);
     }
 }
