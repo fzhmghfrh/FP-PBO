50d768143f3f5eb284777cd9f1286a827b2f1286
Kartika Diva Asmara Gita <90994290+divaasmara@users.noreply.github.com>
2022-12-18 14:24:14 +0700

    Update Maze.java

--- FinalProjectPacman/src/application/Maze.java
+++ FinalProjectPacman/src/application/Maze.java
@@ -32,5 +32,37 @@ public class Maze {
         }
         return false;
     }
-
+    
+    /**
+     * lets you know if there's an obstacle in the current coordinate
+     * @param fromX
+     * @param toX
+     * @param fromY
+     * @param toY
+     * @return
+     */
+    public Boolean hasObstacle(double fromX,  double toX, double fromY, double toY) {
+        boolean isTouching = false;
+        for (double i = fromX; i < toX; i++) {
+            for (double j = fromY; j < toY; j++) {
+                if (this.isTouching(i, j, 0)) isTouching = true;
+            }
+        }
+        return isTouching;
+    }
+    
+    /**
+     * Draws the maze
+     * @param root
+     */
+    public void CreateMaze(Group root) {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~ frame ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // top
+        this.obstacles.add(new BarObstacle(0, 0, "horizontal", 48));
+        // bottom
+        this.obstacles.add(new BarObstacle(0, 600, "horizontal", 48));
+        // left
+        this.obstacles.add(new BarObstacle(0, 0, "vertical", 25));
+        // right
+        this.obstacles.add(new BarObstacle(1225 - BarObstacle.THICKNESS, 0, "vertical", 25));
 }
