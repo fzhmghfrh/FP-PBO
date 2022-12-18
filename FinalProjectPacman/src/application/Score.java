1132e7f06f7c161ba33b340c75206d16e8059e95
fzhmghfrh <120808179+fzhmghfrh@users.noreply.github.com>
2022-12-18 18:10:18 +0700

    Update Score.java
    add Top Score feature

--- FinalProjectPacman/src/application/Score.java
+++ FinalProjectPacman/src/application/Score.java
@@ -8,16 +8,24 @@ import javafx.scene.text.Text;
 public class Score {
     public Text score;
     public Text lifes;
+    public Text topscore;
 
     Score(Group root) {
         this.score = new Text(BarObstacle.THICKNESS * 4, BarObstacle.THICKNESS * 26.5, "Score: ");
         this.lifes = new Text(BarObstacle.THICKNESS * 20, BarObstacle.THICKNESS * 26.5,"Lifes: 3");
+        this.topscore = new Text(BarObstacle.THICKNESS * 35, BarObstacle.THICKNESS * 26.5,"Top Score: 0");
+        
         score.setFill(Color.MAROON);
         score.setFont(Font.font("Arial", 30));
+        
         lifes.setFill(Color.MAROON);
         lifes.setFont(Font.font("Arial", 30));
+        
+        topscore.setFill(Color.MAROON);
+        topscore.setFont(Font.font("Arial", 30));
 
         root.getChildren().add(score);
         root.getChildren().add(lifes);
+        root.getChildren().add(topscore);
     }
 }
