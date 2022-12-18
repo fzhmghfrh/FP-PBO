cfedac31b67769f8f4679e02a4021011113c1099
Kartika Diva Asmara Gita <90994290+divaasmara@users.noreply.github.com>
2022-12-18 14:31:36 +0700

    Update Score.java

--- FinalProjectPacman/src/application/Score.java
+++ FinalProjectPacman/src/application/Score.java
@@ -6,5 +6,12 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Score {
+    public Text score;
+    public Text lifes;
 
+    Score(Group root) {
+        this.score = new Text(BarObstacle.THICKNESS * 4, BarObstacle.THICKNESS * 26.5, "Score: ");
+        this.lifes = new Text(BarObstacle.THICKNESS * 20, BarObstacle.THICKNESS * 26.5,"Lifes: 3");
+        score.setFill(Color.MAROON);
+        score.setFont(Font.font("Arial", 30));
 }
