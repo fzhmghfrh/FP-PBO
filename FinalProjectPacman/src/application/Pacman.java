6a64a93c954a4abce5e9700de8a0f70000d7d284
shafanh <120880399+shafanh@users.noreply.github.com>
2022-12-18 15:44:50 +0700

    Update Pacman.java

--- FinalProjectPacman/src/application/Pacman.java
+++ FinalProjectPacman/src/application/Pacman.java
@@ -7,5 +7,7 @@ public class Pacman extends Circle {
     public Pacman(double x, double y) {
         this.setCenterX(x);
         this.setCenterY(y);
+        this.setRadius(25);
+        this.setFill(Color.YELLOW);
     }
 }
